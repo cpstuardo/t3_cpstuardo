5ad86948abc74d2261951fddc29f7cce20650708
cpstuardo <cpstuardo@uc.cl>
2021-05-22 23:06:36 -0400

    deploy 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import socket from './components/Socket';
 import React, {useState} from 'react';
-import TextField from '@material-ui/core/TextField' ;
-import { useEffect } from 'react/cjs/react.development';
 import './App.css';
 
 
