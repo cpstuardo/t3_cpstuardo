e7d86d43b48d47fa6d824c42e5b5cdddb6d09723
cpstuardo <cpstuardo@uc.cl>
2021-05-22 22:18:09 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <div className = "header">
+        <h1>Bienvenido a Vuelos</h1>
+        <form>
+          <button>Solicitar información de vuelos</button>
+        </form>
+      </div>
     </div>
   );
 }
